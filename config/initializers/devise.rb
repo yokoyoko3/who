9ae29201fc80f93bbbfb801fe217dea2c4681980
yokoyoko3 <mr.yokoyoko3@gmail.com>
2020-02-11 20:26:03 +0900

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,8 +260,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  # config.omniauth :facebook,ENV['FACEBOOK_KEY'],ENV['FACEBOOK_SECRET'],scope: 'email',info_fields: 'email',callback_url: "https://who-who-who.herokuapp.com/users/auth/facebook/callback"
-  # config.omniauth :google_oauth2,ENV['GOOGLE_KEY'],ENV['GOOGLE_SECRET'],scope: 'email',info_fields: 'email',callback_url: "https://who-who-who.herokuapp.com/users/auth/google/callback"
+ 
   config.omniauth :facebook,ENV['FACEBOOK_CLIENT_ID'],ENV['FACEBOOK_CLIENT_SECRET']
   config.omniauth :google_oauth2,ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET']
 
