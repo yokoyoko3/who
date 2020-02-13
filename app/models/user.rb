2dff1e5f7488a25bbc1425dd8dbe502811aa5b7a
yokoyoko3 <mr.yokoyoko3@gmail.com>
2020-02-13 13:13:28 +0900

    readme

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, uniqueness: true
   has_many :sns_credentials
-  has_many :persons
+  has_many :persons, dependent: :destroy
   def self.from_omniauth(auth)
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
     user = sns.user || User.where(email: auth.info.email).first_or_initialize(
